944da3f58bf66a0b1e12ded902ff803890c705fa
AishwaryaSheelvant <112598766+AishwaryaSheelvant@users.noreply.github.com>
2022-12-01 12:37:06 -0500

    Update ls1.py
    With new read graph function

--- py/ls1.py
+++ py/ls1.py
@@ -3,16 +3,22 @@ from random import choice, seed
 from itertools import chain
 from utils import construct_graph
 import networkx as nx 
-
+import os
+output_directory = './output/' 
+if not os.path.exists(output_directory):
+    os.makedirs(output_directory)
 def fast_vc(graph, filename, cutoff_time, random_seed):
     seed(random_seed)
-    edges=graph.edges()
-    vertex_cover = [0] * (max(graph.nodes()) + 1)
+    # edges=graph.edges()
+    # vertex_cover = [0] * (max(graph.nodes()) + 1)
+    edges=list(set(chain(
+        *[[(u, v) if u < v else (v, u) for v in graph[u]] for u in graph])))
+    vertex_cover = [0] * (max(graph) + 1)
     for i, j in edges:
         if vertex_cover[i] + vertex_cover[j] == 0:
             ind=max(i, j, key=lambda x: len(graph[x]))
             vertex_cover[ind] = 1
-    print(edges)
+    # print(edges)
     loss = [0] * len(vertex_cover) #loss for each vertex
     for i, j in edges:
         if vertex_cover[i] + vertex_cover[j] == 1: #picking vertex which exists in the vertex cover
@@ -35,10 +41,13 @@ def fast_vc(graph, filename, cutoff_time, random_seed):
     # IO stuff
     start_time = cur_time = datetime.now()
     inf = float('inf')
-    base = filename.split('/')[-1].split('.')[0] \
-           + '_LS2_' + str(cutoff_time) + '_' \
+    out = filename.split('/')[-1].split('.')[0] \
+           + '_ls1_' + str(cutoff_time) + '_' \
            + str(random_seed)
-    calc=calculation(start_time,cutoff_time,graph,vertex_cover,base,gain,loss,edges)
+    base=output_directory+out
+    print(base)
+    # base=open(os.path.join(output_directory, base),'w')
+    calc=calculation(start_time,cutoff_time,graph,vertex_cover,out,gain,loss,edges)
     with open(base + '.sol', 'w') as sol:
         sol.write(str(sum(calc)) + '\n')
         sol.write(
@@ -49,11 +58,12 @@ def fast_vc(graph, filename, cutoff_time, random_seed):
 # Run the algorithm on an input graph with a specified time and random seed
 def run(filename, cutoff_time, random_seed):
     seed(random_seed)
-    graph,n = construct_graph(filename)
+    graph = read_graph(filename)
     fast_vc(graph, filename, cutoff_time, random_seed)
-def calculation(start_time,cutoff_time,graph,vertex_cover,base,gain,loss,edges):
+def calculation(start_time,cutoff_time,graph,vertex_cover,out,gain,loss,edges):
     inf = float('inf')
     cur_time = datetime.now()
+    base=output_directory+out
     with open(base + '.trace', 'w') as trace:
         while cur_time - start_time < timedelta(seconds=cutoff_time):
                 is_solution=True
@@ -109,3 +119,15 @@ def calculation(start_time,cutoff_time,graph,vertex_cover,base,gain,loss,edges):
                         loss[v] -= 1
                 cur_time = datetime.now()
     return calc
+def read_graph(filename):
+    graph = {}
+    with open(filename, 'r') as file:
+        file.readline()
+        for i, line in enumerate(file):
+            if len(line.strip()) > 0:
+                l=[]
+                for j in line.split():
+                    l.append(int(j)-1)
+                graph[i]=set(l)
+    return graph
+
