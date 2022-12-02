f561b29c90f160d2719b2e1cf56aed03e70e6163
keerthan2 <keerthan2@gmail.com>
2022-12-02 12:17:26 -0500

    changed utils

--- py/main.py
+++ py/main.py
@@ -57,9 +57,9 @@ def solve(instance, method, cutoff, rand_seed):
     elif method == 'ls1':
         ls1.run(instance, cutoff, rand_seed)
     elif method == 'ls2':
-        G, nNodes = construct_graph(instance)
+        G = construct_graph(instance)
         VC_opt, return_str = SimulatedAnnealing(G, cutoff, start_time, return_str = "", seed = rand_seed)
-        write_results(VC_opt, return_str, nNodes, output_directory, instance_name, method, cutoff, rand_seed)
+        write_results(VC_opt, return_str, output_directory, instance_name, method, cutoff, rand_seed)
     else:
         print("Invalid method entered. Please enter one of [bnb|approx|ls1|ls2].")
 
